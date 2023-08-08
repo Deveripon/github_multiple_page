06b559852f99004847eb7deab58cd5e51672e5f4
deveripon <developershahadathussainripon@gmail.com>
2023-08-08 14:00:02 +0600

    Checked github multiple page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
  function showPage(pageName) {
 
-   window.location.replace = `https://deveripon.github.io/github_multiple_page/${pageName}.html`;
+   window.location.pathname = `https://deveripon.github.io/github_multiple_page/${pageName}.html`;
 
  }
 
  function showPortfolioPage() {
-   window.location.href = `https://deveripon.github.io/github_multiple_page/public/${pageName}.html`;
+   window.location.pathname = `https://deveripon.github.io/github_multiple_page/public/${pageName}.html`;
  }
